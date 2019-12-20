f424a492daee5400ad3f115e8a76909b3c5aded0
Felipe Fernandes dos Santos <felipe@deltacon.com.br>
2019-12-20 17:48:16 -0300

    Movido classe AccountLogon para outro arquivo

--- QACoreBusiness/Util/AbrirNavegadorUtil.cs
+++ QACoreBusiness/Util/AbrirNavegadorUtil.cs
@@ -7,15 +7,7 @@ using Xunit;
 
 namespace QACoreBusiness.Util
 {
-    public class AccountLogon
-    {
-        public string URL => "http://192.168.0.2/COREBusiness/Account/LogOn";
-        public IWebDriver Driver;
-        public IWebElement Usuario => Driver.FindElement(By.Id("Username"));
-        public IWebElement Senha => Driver.FindElement(By.Id("Password"));
-        public IWebElement BotaoEfetuarLogin => Driver.FindElement(By.XPath("//button[@type='submit'][@name='action']"));
-    }
-  
+
     public class AbrirNavegadorUtil
     {
         AccountLogon tela;
---
+++ QACoreBusiness/Util/AccountLogon.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QACoreBusiness.Util
+{
+    class AccountLogon
+    {
+        public string URL => "http://192.168.0.2/COREBusiness/Account/LogOn";
+
+        public IWebDriver Driver;
+        public IWebElement Usuario => Driver.FindElement(By.Id("Username"));
+        public IWebElement Senha => Driver.FindElement(By.Id("Password"));
+        public IWebElement BotaoEfetuarLogin => Driver.FindElement(By.XPath("//button[@type='submit'][@name='action']"));
+    }
+}
